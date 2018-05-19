7d0c4642995ffd1e76d0e7ac7f3a2f7350f7dc77
jellywen <jellywen@jellywendeMacBook-Pro.local>
2018-05-20 00:35:11 +0800

    入口文件

--- backend/web/index.php
+++ backend/web/index.php
@@ -12,8 +12,8 @@ require __DIR__ . '/../config/bootstrap.php';
 $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../../common/config/main.php',
     require __DIR__ . '/../config/main.php',
-    require __DIR__ . '/environments/' . YII_ENV . '/common/config/main-local.php',
-    require __DIR__ . '/environments/' . YII_ENV . '/console/config/main-local.php'
+    require __DIR__ . '/../../environments/' . YII_ENV . '/common/config/main-local.php',
+    require __DIR__ . '/../../environments/' . YII_ENV . '/backend/config/main-local.php'
 );
 
 (new yii\web\Application($config))->run();
