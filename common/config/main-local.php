26ebe36be1535d6c35f9be0b3f9a0115992b18cb
jellywen <jellywen@koudailc.com>
2018-05-19 22:23:59 +0800

    add prod env

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=172.31.231.210;dbname=jelly',
             'username' => 'root',
-            'password' => '',
+            'password' => 'mysql_prod',
             'charset' => 'utf8',
         ],
         'mailer' => [
