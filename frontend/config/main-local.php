b071358b25c9e28e37f2916b5aa90beba196dc61
jellywen <jellywen@koudailc.com>
2018-05-19 21:42:57 +0800

    初始化为生产环境 php init --env=Production --overwrite=All

---
+++ frontend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '8LQmLbgGvD6IW6Jt5KcbEQgcME7bbTy8',
+        ],
+    ],
+];
